87c3bd0345e3b48d1c34fd2510b6e7e1c36f33fe
GALLUCCIO Francesco <francesco.galluccio@amaris.com>
2025-12-23 09:32:09 +0100

    test Playwright

--- pytest_playwright/test_messaggi_09.py
+++ pytest_playwright/test_messaggi_09.py
@@ -34,17 +34,16 @@ def test_etichetta(page):
     time.sleep(2)
     # Salva nuova etichetta
     page.get_by_role("button", name="Salva").click()
-
-    chiudi = page.locator(
-    "div.aru-tooltip.light button[title='Chiudi']"
-)
+    time.sleep(2)
+    
+    chiudi = page.get_by_role("button", name="Chiudi")
 
     try:
         chiudi.wait_for(state="visible", timeout=3000)
         chiudi.click()
     except:
         pass
-    
+
     # Seleziona tutti i messaggi
     page.locator('div.aru-input-checkbox').nth(1).click()
     time.sleep(2)
